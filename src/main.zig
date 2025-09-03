60c9c1c3f6e7cb7dbe5c82054d6ab47af0a8966f
Kevin Nguyen <ngynkvn@gmail.com>
2025-09-02 23:58:31 -0400

    wip query

--- src/main.zig
+++ src/main.zig
@@ -14,14 +14,22 @@ pub fn main() !void {
         try s.writeAll(E.HOME);
         try s.writeAll("Hello, world!\n");
         try s.print("{?}\n", .{last_event});
+        try s.query();
+        while (s.pollEvent()) |event| {
+            switch (event) {
+                .key => |key| {
+                    last_event = event;
+                    switch (key) {
+                        .q, .Q => s.running = false,
+                        else => {},
+                    }
+                },
+                .cursor_pos => |cursor_pos| {
+                    try s.print("{}\n", .{cursor_pos});
+                },
+            }
+        }
         try s.flush();
         std.Thread.sleep(std.time.ns_per_s / 16);
-        last_event = s.pollEvent() orelse continue;
-        switch (last_event.?) {
-            .KeyPress => |key| switch (key) {
-                .q, .Q => s.running = false,
-                else => {},
-            },
-        }
     }
 }

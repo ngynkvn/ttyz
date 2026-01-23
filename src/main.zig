04df72e3ee7d75b3403019d8af6cbedc87a3a3e6
Kevin Nguyen <ngynkvn@gmail.com>
2026-01-23 09:20:24 -0500

    feat: add Kitty graphics protocol implementation for terminal images

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,18 @@ const ttyz = @import("ttyz");
 const E = ttyz.E;
 
 pub fn main() !void {
+    const tty = try std.fs.openFileAbsolute("/dev/tty", .{ .mode = .read_write });
+    var image = ttyz.kitty.Image.default;
+    image.params.a = 'T';
+    image.params.t = 'f';
+    image.params.f = 100;
+    image.filePath("testdata/mushroom.png");
+    var w = tty.writer(&.{});
+    try image.write(&w.interface);
+    try w.interface.flush();
+    {
+        return;
+    }
     var s = try ttyz.Screen.init();
     defer _ = s.deinit() catch |e| {
         std.log.err("Error deinitializing raw mode: {s}", .{@errorName(e)});
@@ -36,6 +48,7 @@ pub fn main() !void {
                 },
             }
         }
+        try s.print(E.ESC ++ "Gf=100,t=f,a=T;{s}" ++ E.ESC ++ "\\", .{"L1VzZXJzL25neW5rdm4vZGV2L3ppZy90dHl6L3Rlc3RkYXRhL211c2hyb29tLnBuZwo="});
         try s.flush();
         std.Thread.sleep(std.time.ns_per_s / 16);
     }

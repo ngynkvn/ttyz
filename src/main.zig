1192fd864d19ca9dda485995e15674636ce18391
Kevin Nguyen <ngynkvn@gmail.com>
2026-01-23 09:20:24 -0500

    input

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,27 @@
 const std = @import("std");
 const ttyz = @import("ttyz");
+const E = ttyz.E;
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try ttyz.bufferedPrint();
-}
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
+    var s = try ttyz.Screen.init();
+    defer _ = s.deinit() catch |e| {
+        std.log.err("Error deinitializing raw mode: {s}", .{@errorName(e)});
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    try s.start();
+    var last_event: ?ttyz.Event = null;
+    while (s.running) {
+        try s.writeAll(E.CLEAR_SCREEN);
+        try s.writeAll(E.HOME);
+        try s.writeAll("Hello, world!\n");
+        try s.print("{?}\n", .{last_event});
+        try s.flush();
+        std.Thread.sleep(std.time.ns_per_s / 16);
+        last_event = s.pollEvent() orelse continue;
+        switch (last_event.?) {
+            .KeyPress => |key| switch (key) {
+                .q, .Q => s.running = false,
+                else => {},
+            },
+        }
+    }
 }
---
+++ src/ttyz.zig
@@ -0,0 +1,292 @@
+const std = @import("std");
+
+const posix = std.posix;
+const system = posix.system;
+
+pub const CONFIG = .{
+    // Disable tty's SIGINT handling,
+    .HANDLE_SIGINT = true,
+    .START_SEQUENCE = E.ENTER_ALT_SCREEN ++ E.CURSOR_INVISIBLE,
+    .EXIT_SEQUENCE = E.EXIT_ALT_SCREEN ++ E.CURSOR_VISIBLE,
+    .TTY_HANDLE = "/dev/tty",
+};
+
+/// vt100 / xterm escape sequences
+/// References used:
+///  - https://vt100.net/docs/vt100-ug/chapter3.html
+///  - `man terminfo`, `man tput`, `man infocmp`
+// zig fmt: off
+pub const E = struct {
+    /// escape code prefix
+    pub const ESC= "\x1b[";
+    pub const HOME               = ESC ++ "H";
+    /// goto .{y, x}
+    pub const GOTO               = ESC ++ "{d};{d}H";
+    pub const CLEAR_LINE         = ESC ++ "K";
+    pub const CLEAR_DOWN         = ESC ++ "0J";
+    pub const CLEAR_UP           = ESC ++ "1J";
+    pub const CLEAR_SCREEN       = ESC ++ "2J"; // NOTE: https://vt100.net/docs/vt100-ug/chapter3.html#ED
+    pub const ENTER_ALT_SCREEN   = ESC ++ "?1049h";
+    pub const EXIT_ALT_SCREEN    = ESC ++ "?1049l";
+    pub const REPORT_CURSOR_POS  = ESC ++ "6n";
+    pub const CURSOR_INVISIBLE   = ESC ++ "?25l";
+    pub const CURSOR_VISIBLE     = ESC ++ "?12;25h";
+    pub const CURSOR_UP          = ESC ++ "{}A";
+    pub const CURSOR_DOWN        = ESC ++ "{}B";
+    pub const CURSOR_FORWARD     = ESC ++ "{}C";
+    pub const CURSOR_BACKWARDS   = ESC ++ "{}D";
+    pub const CURSOR_HOME_ROW    = ESC ++ "1G";
+    pub const CURSOR_COL_ABS     = ESC ++ "{}G";
+    pub const CURSOR_SAVE_POS    = ESC ++ "7";
+    pub const CURSOR_RESTORE_POS = ESC ++ "8";
+    /// setaf .{color}
+    pub const SET_ANSI_FG        = ESC ++ "3{d}m";
+    /// setab .{color}
+    pub const SET_ANSI_BG        = ESC ++ "4{d}m";
+    /// set true color (rgb)
+    pub const SET_TRUCOLOR       = ESC ++ "38;2;{};{};{}m";
+    pub const RESET_COLORS       = ESC ++ "m";
+};
+const cc = std.ascii.control_code;
+// zig fmt: on
+
+pub const TtyHandle = struct {};
+
+pub const Screen = struct {
+    orig_termios: posix.termios,
+    tty: std.fs.File,
+    width: u16,
+    height: u16,
+    buffer: [4096]u8,
+    input_buffer: [4096]u8,
+    last_read: []u8,
+    lock: std.Thread.Mutex,
+    writer: std.fs.File.Writer,
+    event_buffer: [32]Event,
+    event_queue: std.ArrayList(Event),
+    io_thread: ?std.Thread,
+    running: bool,
+    pub const Error = std.posix.WriteError;
+    pub const CursorPos = struct { row: usize, col: usize };
+
+    /// Enter "raw mode", returning a struct that wraps around the provided tty file
+    /// Entering raw mode will automatically send the sequence for entering an
+    /// alternate screen (smcup) and hiding the cursor.
+    /// Use `defer Screen.deinit()` to reset on exit.
+    /// Deferral will set the sequence for exiting alt screen (rmcup)
+    ///
+    /// Explanation here: https://viewsourcecode.org/snaptoken/kilo/02.enteringScreen.html
+    /// https://zig.news/lhp/want-to-create-a-tui-application-the-basics-of-uncooked-terminal-io-17gm
+    pub fn init() !Screen {
+        const tty = try std.fs.openFileAbsolute(CONFIG.TTY_HANDLE, .{ .mode = .read_write });
+        return try initFrom(tty);
+    }
+
+    pub fn initFrom(tty: std.fs.File) !Screen {
+        const orig_termios = try posix.tcgetattr(tty.handle);
+        var raw = orig_termios;
+        // Some explanation of the flags can be found in the links above.
+        // TODO: check out the other flags later
+        // zig fmt: off
+        raw.lflag.ECHO   = false;                 // Disable echo input
+        raw.lflag.ICANON = false;                 // Read byte by byte
+        raw.lflag.IEXTEN = false;                 // Disable <C-v>
+        raw.lflag.ISIG   = !CONFIG.HANDLE_SIGINT; // Disable <C-c> and <C-z>
+        raw.iflag.IXON   = false;                 // Disable <C-s> and <C-q>
+        raw.iflag.ICRNL  = false;                 // Disable <C-m>
+        raw.iflag.BRKINT = false;                 // Break condition sends SIGINT
+        raw.iflag.INPCK  = false;                 // Enable parity checking
+        raw.iflag.ISTRIP = false;                 // Strip 8th bit of input byte
+        raw.oflag.OPOST  = false;                 // Disable translating "\n" to "\r\n"
+        raw.cflag.CSIZE  = .CS8;
+
+        raw.cc[@intFromEnum(system.V.MIN)]  = 0;  // min bytes required for read
+        raw.cc[@intFromEnum(system.V.TIME)] = 1;  // min time to wait for response, 100ms per unit
+        // zig fmt: on
+
+        const rc = system.tcsetattr(tty.handle, .FLUSH, &raw);
+        if (posix.errno(rc) != .SUCCESS) return error.CouldNotSetTermiosFlags;
+
+        // IOCGWINSZ (io control get window size (?)) is a request signal for window size
+        var ws: posix.winsize = .{ .row = 0, .col = 0, .xpixel = 0, .ypixel = 0 };
+        // Get the window size via ioctl(2) call to tty
+        const result = system.ioctl(tty.handle, posix.T.IOCGWINSZ, @intFromPtr(&ws));
+        if (posix.errno(result) != .SUCCESS) return error.IoctlReturnedNonZero;
+
+        const width = ws.col;
+        const height = ws.row;
+        std.log.debug("windowsize is {}x{}; xpixel={d}, ypixel={d}", .{ width, height, ws.xpixel, ws.ypixel });
+        _ = try tty.write(CONFIG.START_SEQUENCE);
+        var self: Screen = undefined;
+        self = .{
+            .orig_termios = orig_termios,
+            .tty = tty,
+            .running = true,
+            .width = width,
+            .height = height,
+            .last_read = &.{},
+            // writer
+            .lock = .{},
+            .buffer = std.mem.zeroes([4096]u8),
+            .writer = tty.writer(&self.buffer),
+            // input
+            .input_buffer = std.mem.zeroes([4096]u8),
+            .io_thread = null,
+            .event_buffer = undefined,
+            .event_queue = std.ArrayList(Event).initBuffer(&self.event_buffer),
+        };
+        return self;
+    }
+
+    pub fn deinit(self: *Screen) !posix.E {
+        self.running = false;
+        if (self.io_thread) |thread| thread.join();
+        _ = try self.tty.write(CONFIG.EXIT_SEQUENCE);
+        const rc = system.tcsetattr(self.tty.handle, .FLUSH, &self.orig_termios);
+        self.tty.close();
+        return posix.errno(rc);
+    }
+
+    pub fn start(self: *Screen) !void {
+        self.io_thread = std.Thread.spawn(.{}, Screen.ioLoop, .{self}) catch |e| {
+            std.log.err("error spawning main loop: {s}", .{@errorName(e)});
+            return e;
+        };
+    }
+
+    pub fn ioLoop(self: *Screen) !void {
+        while (self.running) {
+            const n = self.read(&self.input_buffer) catch continue;
+            self.last_read = self.input_buffer[0..n];
+            self.collectEvents();
+        }
+    }
+    pub fn collectEvents(self: *Screen) void {
+        if (self.last_read.len == 0) return;
+        var ll = self.last_read;
+        var event: ?Event = null;
+        switch (ll[0]) {
+            'a'...'z', 'A'...'Z', '0'...'9' => |c| {
+                event = .{ .KeyPress = @enumFromInt(c) };
+            },
+            cc.esc => {
+                ll = ll[1..];
+                if (ll.len == 0) {
+                    event = .{ .KeyPress = .esc };
+                    return;
+                }
+                switch (ll[0]) {
+                    '[' => switch (ll[1]) {
+                        'A', 'B', 'C', 'D' => |c| {
+                            event = .{ .KeyPress = .arrow(c) };
+                        },
+                        else => {},
+                    },
+                    else => {},
+                }
+            },
+            else => {},
+        }
+        if (event) |e| self.event_queue.appendBounded(e) catch {};
+    }
+    pub fn pollEvent(self: *Screen) ?Event {
+        if (self.event_queue.items.len == 0) return null;
+        return self.event_queue.orderedRemove(0);
+    }
+
+    /// Move cursor to (x, y) (column, row)
+    /// (0, 0) is defined as the bottom left corner of the terminal.
+    pub fn goto(self: *Screen, r: u16, c: u16) !void {
+        self.lock.lock();
+        defer self.lock.unlock();
+        try self.print(E.GOTO, .{ r, c });
+    }
+
+    pub fn query(self: *Screen) !CursorPos {
+        self.lock.lock();
+        defer self.lock.unlock();
+        _ = try self.tty.write(E.REPORT_CURSOR_POS);
+        // TODO: make this more durable
+        var buf: [32]u8 = undefined;
+        const n = try self.tty.read(&buf);
+        if (!std.mem.startsWith(u8, &buf, E.ESC)) return error.UnknownResponse;
+        const semi = std.mem.indexOf(u8, &buf, ";") orelse return error.ParseError;
+        const row = try std.fmt.parseUnsigned(usize, buf[2..semi], 10);
+        const col = try std.fmt.parseUnsigned(usize, buf[semi + 1 .. n - 1], 10);
+        return .{ .row = row, .col = col };
+    }
+
+    /// read input
+    pub fn read(self: *Screen, buffer: []u8) !usize {
+        return self.tty.read(buffer);
+    }
+
+    /// print to screen via fmt string
+    pub fn print(self: *Screen, comptime fmt: []const u8, args: anytype) !void {
+        try self.printArgs(fmt, args, .{});
+    }
+
+    /// raw write
+    pub fn write(self: *Screen, buf: []const u8) !usize {
+        self.lock.lock();
+        defer self.lock.unlock();
+        return try self.writer.interface.write(buf);
+    }
+
+    pub fn writeAll(self: *Screen, buf: []const u8) !void {
+        self.lock.lock();
+        defer self.lock.unlock();
+        try self.writer.interface.writeAll(buf);
+    }
+
+    pub const WriteArgs = struct { cursor: enum { KEEP, RESTORE_POS } = .KEEP, sleep: usize = 0 };
+    pub fn printArgs(self: *Screen, comptime fmt: []const u8, args: anytype, wargs: WriteArgs) !void {
+        self.lock.lock();
+        defer self.lock.unlock();
+        // TODO: check if this will exclude this code from being added at comptime
+        if (wargs.sleep != 0) std.Thread.sleep(wargs.sleep);
+        _ = if (wargs.cursor == .RESTORE_POS) try self.writer.interface.writeAll(E.CURSOR_SAVE_POS);
+        try self.writer.interface.print(fmt, args);
+        _ = if (wargs.cursor == .RESTORE_POS) try self.writer.interface.writeAll(E.CURSOR_RESTORE_POS);
+    }
+
+    pub fn flush(self: *Screen) !void {
+        self.lock.lock();
+        defer self.lock.unlock();
+        return self.writer.interface.flush();
+    }
+};
+
+pub const Event = union(enum) {
+    // ASCII letters
+    pub const Key = enum(u8) {
+        esc = 27,
+        arrow_up,
+        arrow_down,
+        arrow_right,
+        arrow_left,
+        // zig fmt: off
+        @"0" = 48, @"1" = 49, @"2" = 50, @"3" = 51, @"4" = 52, @"5" = 53, @"6" = 54, @"7" = 55, @"8" = 56, @"9" = 57,
+
+        A = 65, B = 66, C = 67, D = 68, E = 69, F = 70, G = 71, H = 72, 
+        I = 73, J = 74, K = 75, L = 76, M = 77, N = 78, O = 79, P = 80, 
+        Q = 81, R = 82, S = 83, T = 84, U = 85, V = 86, W = 87, X = 88, Y = 89, Z = 90,
+
+        a = 97, b = 98, c = 99, d = 100, e = 101, f = 102, g = 103, h = 104, 
+        i = 105, j = 106, k = 107, l = 108, m = 109, n = 110, o = 111, p = 112, 
+        q = 113, r = 114, s = 115, t = 116, u = 117, v = 118, w = 119, x = 120, y = 121, z = 122,
+
+        _,
+        // zig fmt: on
+        fn arrow(c: u8) Key {
+            return switch (c) {
+                'A' => .arrow_up,
+                'B' => .arrow_down,
+                'C' => .arrow_right,
+                'D' => .arrow_left,
+                else => unreachable,
+            };
+        }
+    };
+    KeyPress: Key,
+};
